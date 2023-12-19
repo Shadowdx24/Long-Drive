5a9b442f4699c239673ba89a058de7484b6f8fc1
Shadowdx24 <110624444+Shadowdx24@users.noreply.github.com>
2023-12-19 17:29:00 +0530

    Added Road & its movement

---
+++ Assets/Scripts/Road.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Road : MonoBehaviour
+{
+    [SerializeField] private Renderer road;
+    [SerializeField] public float speed=0.1f;
+    private Vector2 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offset=new Vector2(0,Time.time*speed);
+        road.material.mainTextureOffset = offset;
+    }
+}
