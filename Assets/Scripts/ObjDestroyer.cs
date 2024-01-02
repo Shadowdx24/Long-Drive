3c7bae40815245e61eb3f2fe54a03049b7abd853
Shadowdx24 <110624444+Shadowdx24@users.noreply.github.com>
2024-01-02 13:56:01 +0530

    Modified Functionality for Destroyer

--- Assets/Scripts/ObjDestroyer.cs
+++ Assets/Scripts/ObjDestroyer.cs
@@ -15,9 +15,13 @@ public class ObjDestroyer : MonoBehaviour
     {
         
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("enemy"))
+        {
+            Destroy(collision.gameObject);
+        }
+        else if(collision.gameObject.CompareTag("fuel"))
         {
             Destroy(collision.gameObject);
         }
